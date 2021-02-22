d2dd5d0c8424de6ee3f72ddbb83de84adaf7389d
Rafi Hossain <arrafi245@gmail.com>
2021-02-22 23:01:12 +0600

    js calculator project

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$('#img1').click(function(){
+    var imageValue=$(this).attr('src');
+    $('#mainImage').attr('src', imageValue);
+});
+
+$('#img2').click(function(){
+    var imageValue=$(this).attr('src');
+    $('#mainImage').attr('src', imageValue);
+});
+
+$('#img3').click(function(){
+    var imageValue=$(this).attr('src');
+    $('#mainImage').attr('src', imageValue);
+});
+
+$('#img4').click(function(){
+    var imageValue=$(this).attr('src');
+    $('#mainImage').attr('src', imageValue);
+});
